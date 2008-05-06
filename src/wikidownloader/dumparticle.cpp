c819222d7214e57a7321e3c458b899aa088875b0
maekitalo <maekitalo@localhost>
2008-05-06 20:11:39 +0000

    add some more tools

---
+++ src/wikidownloader/dumparticle.cpp
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2008 Tommi Maekitalo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
+ * NON-INFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ */
+
+#include <iostream>
+#include <tntdb/connect.h>
+#include <tntdb/row.h>
+#include <tntdb/blob.h>
+#include <tntdb/statement.h>
+#include <zeno/article.h>
+#include <cxxtools/convert.h>
+#include <cxxtools/loginit.h>
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    log_init();
+
+    tntdb::Connection conn = tntdb::connect("postgresql:dbname=zeno");
+
+    tntdb::Statement selArticle = conn.prepare(
+      "select title, compression, data, redirect"
+      "  from article"
+      " where aid = :aid");
+
+    for (int a = 1; a < argc; ++a)
+    {
+      unsigned n = cxxtools::convert<unsigned>(argv[a]);
+      tntdb::Row row = selArticle.set("aid", n)
+                                 .selectRow();
+      zeno::Article article;
+      article.setTitle(zeno::QUnicodeString(row.getString(0)));
+      article.setCompression(static_cast<zeno::Dirent::CompressionType>(row.getInt(1)));
+      tntdb::Blob data;
+      row.getBlob(2, data);
+      article.setRawData(std::string(data.data(), data.size()));
+      article.setRedirectFlag(!row.isNull(3));
+
+      std::cout << "aid=" << n << " title=" << article.getTitle() << " redirect=" << article.getRedirectFlag() << '\n'
+        << article.getData() << std::endl;
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
---
+++ src/wikidownloader/trycompress.cpp
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2008 Tommi Maekitalo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
+ * NON-INFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ */
+
+#include <iostream>
+#include <tntdb/connect.h>
+#include <tntdb/row.h>
+#include <tntdb/blob.h>
+#include <tntdb/statement.h>
+#include <zeno/article.h>
+#include <cxxtools/convert.h>
+#include <cxxtools/loginit.h>
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    log_init();
+
+    tntdb::Connection conn = tntdb::connect("postgresql:dbname=zeno");
+
+    tntdb::Statement selArticle = conn.prepare(
+      "select aid, title, data"
+      "  from article"
+      " where compression is null"
+      "   and data is not null"
+      "   and redirect is null");
+
+    tntdb::Statement updArticle = conn.prepare(
+      "update article"
+      "   set compression = :compression,"
+      "       data = :data"
+      " where aid = :aid");
+
+    for (tntdb::Statement::const_iterator cur = selArticle.begin();
+         cur != selArticle.end(); ++cur)
+    {
+      tntdb::Row row = *cur;
+      unsigned aid      = row[0].getUnsigned();
+      std::string title = row[1].getString();
+      tntdb::Blob data  = row[2].getBlob();
+
+      zeno::Article article;
+      article.setRawData(std::string(data.data(), data.size()));
+      article.setCompression(zeno::Dirent::zenocompNone);
+      article.tryCompress();
+      std::cout << "article " << aid << ": " << title << "\t=> compression " << article.getCompression() << std::endl;
+      updArticle.set("aid", aid)
+                .set("compression", static_cast<unsigned>(article.getCompression()))
+                .set("data", tntdb::Blob(article.getRawData().data(), article.getRawData().size()))
+                .execute();
+    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+}
+
